3ea73542703fd5a0f104d044a90faf457a67ac0d
najaroen <najaroen_tik@live.com>
2025-10-28 14:38:26 +0700

    add test-cov

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,16 +1,40 @@
 // jest.config.cjs
-const nextJest = require('next/jest');
+const nextJest = require("next/jest");
 
-const createJestConfig = nextJest({ dir: './' });
+const createJestConfig = nextJest({ dir: "./" });
 
 /** @type {import('jest').Config} */
 const customJestConfig = {
-  testEnvironment: 'jest-environment-jsdom',
-  setupFilesAfterEnv: ['<rootDir>/test/setupTests.ts'],
+  collectCoverage: true,
+  testMatch: ["**/__tests__/**/*.test.[jt]s?(x)"],
+  testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["<rootDir>/test/setupTests.ts"],
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx}",
+    "components/**/*.{ts,tsx}",
+    "app/**/*.{ts,tsx}",
+    "!**/__tests__/**",
+    "!**/*.d.ts",
+    "!**/*test.*",
+    "!**/*spec.*",
+    "!**/node_modules/**",
+    "!**/.next/**",
+    "!**/out/**",
+    "!**/coverage/**",
+    "!**/public/**",
+    "!**/__mocks__/**",
+    "!**/e2e/**",
+    "!**/*config.{js,cjs,mjs,ts}",
+    "!**/next.config.*",
+    "!**/postcss.config.*",
+    "!**/tailwind.config.*",
+    "!**/edge-runtime-*.js",
+    "!**/*manifest*.js",
+  ],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
+    "^@/(.*)$": "<rootDir>/$1",
   },
-  testMatch: ['**/__tests__/**/*.(test|spec).(ts|tsx)'],
+  testMatch: ["**/__tests__/**/*.(test|spec).(ts|tsx)"],
 };
 
 module.exports = createJestConfig(customJestConfig);
