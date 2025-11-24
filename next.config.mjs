672f15f5a126c498cd72dabd2ac20427298f1f27
najaroen <najaroen_tik@live.com>
2025-11-24 11:04:18 +0700

    img

--- next.config.mjs
+++ next.config.mjs
@@ -7,10 +7,11 @@ const nextConfig = {
     return config;
   },
   images: {
+    unoptimized: true,
     domains: [
-      "raw.seadn.io", 
+      "raw.seadn.io",
       "images.unsplash.com",
-      "orange-tremendous-wallaby-734.mypinata.cloud"
+      "orange-tremendous-wallaby-734.mypinata.cloud",
     ],
   },
 };
