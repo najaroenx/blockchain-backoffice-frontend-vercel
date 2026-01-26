7c50805d0d4141a635f12ba0fd0359ded5d42f9a
najaroen <najaroen_tik@live.com>
2026-01-27 03:12:58 +0700

    Fix layout hydration error and integrate user-dashboard API

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,23 +26,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/ais-logo.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/ais-logo.png"
+        />
+      </head>
       <StyledEngineProvider injectFirst>
         <CssBaseline />
         <body className={inter.className}>
-          <header>
-            <link
-              rel="icon"
-              type="image/png"
-              sizes="32x32"
-              href="/images/ais-logo.png"
-            />
-            <link
-              rel="icon"
-              type="image/png"
-              sizes="16x16"
-              href="/images/ais-logo.png"
-            />
-          </header>
           <main>
             <LoadingProvider>
               <SessionWrapper>
