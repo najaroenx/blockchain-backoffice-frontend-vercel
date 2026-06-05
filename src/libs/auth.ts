a45b2484ee51c3432bf6e933a84a0ef35abd55fb
najaroen <najaroen_tik@live.com>
2026-06-05 14:58:50 +0700

    chore: remove unnecessary blank line in auth.ts

--- src/libs/auth.ts
+++ src/libs/auth.ts
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/app/api/auth/options";
-
+// test
 export const getSessionToken = async () => {
   const session = await getServerSession(authOptions);
   return session?.user.accessToken;
